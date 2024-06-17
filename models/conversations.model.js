f4511dfe165bf55435b2696fa7daa50e2676820f
Vineeth <113677394+SecretUsrname@users.noreply.github.com>
2024-06-17 17:45:02 +0530

    Update conversations.model.js

--- models/conversations.model.js
+++ models/conversations.model.js
@@ -1,1 +1,33 @@
+const mongoose = require('mongoose');
 
+const conversations = mongoose.Schema(
+    {
+        ticket_id: {
+            type: String,
+            required: true,
+            ref: 'tickets'
+        },
+
+        tech_exp_id:{
+            type: String,
+            required: true,
+        },
+
+        response: {
+            type: String,
+            required: true,
+        },
+        reason_of_closing: {
+            type: String,
+            required: false,
+        },
+
+    },
+    {
+        timestamps: false,
+    }
+);
+
+const Conversation = mongoose.model("Conversation", conversations);
+
+module.exports = Conversation;
