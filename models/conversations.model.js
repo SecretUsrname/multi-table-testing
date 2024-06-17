a708bde696f32051cbefb4a278ef28282379f0cb
Vineeth <113677394+SecretUsrname@users.noreply.github.com>
2024-06-17 17:50:01 +0530

    Update conversations.model.js

--- models/conversations.model.js
+++ models/conversations.model.js
@@ -2,26 +2,34 @@ const mongoose = require('mongoose');
 
 const conversations = mongoose.Schema(
     {
+        //Reference to ticket_id
         ticket_id: {
             type: String,
             required: true,
             ref: 'tickets'
         },
-
+        // the experts id which we need to add
         tech_exp_id:{
             type: String,
             required: true,
         },
-
+        // response given by the expert
         response: {
-            type: String,
+            //multiple responses while guiding for a solution
+            type: [String],
             required: true,
         },
+        // the reason which is optional that is given by the expert while closing
         reason_of_closing: {
             type: String,
             required: false,
         },
-
+        //optional
+        urls:{
+            //multiple urls at multiple stages of guidance
+            type: [String],
+            required: false
+        },
     },
     {
         timestamps: false,
