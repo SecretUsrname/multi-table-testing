4cb34ad76932a836dbf0da4723c4d9b148b32e84
Vineeth <113677394+SecretUsrname@users.noreply.github.com>
2024-06-18 15:16:02 +0530

    Update techexp.model.js

--- models/techexp.model.js
+++ models/techexp.model.js
@@ -1,1 +1,31 @@
+import mongoose from 'mongoose';
 
+const TechExpSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        EmployeeID: {
+            //If the company has assigned any special number.
+            type: String,
+            required: true,
+        },
+        role:{
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const TechExp = mongoose.model("TechExp", TechExpSchema);
+
+export default TechExp;
